b36a4aa35bf7d5e483a41a64d0361ab39ed78edd
Gaab-elle <ribeiro.gabrielle989@gmail.com>
2025-09-07 08:22:46 -0300

    Add automatic migrations to Railway startup
    - Added php artisan migrate --force to start.sh
    - Will create database tables automatically on startup
    - Should resolve database connection errors
    - Tables will be created before server starts

--- start.sh
+++ start.sh
@@ -8,6 +8,10 @@ php artisan route:clear
 php artisan config:clear
 php artisan cache:clear
 
+# Run migrations
+echo "Running migrations..."
+php artisan migrate --force
+
 # Start server
 echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=$PORT
